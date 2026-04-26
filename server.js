910072564786b0f90c366d422af57f6ba4b90ab1
Amozol254 <ombazaamos@gmail.com>
2026-04-26 03:39:16 +0300

    Integrate MongoDB connection with mongoose
    Added MongoDB connection using mongoose.

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected ✅"))
+.catch(err => console.log("MongoDB Error ❌", err));
 // BetPro Backend API - Working Render Version
 
 const express = require('express');
